8e67194cbbcde253bd93b614b11c1abe25455951
Tomas Maggi <tomimaggi2005@gmail.com>
2024-11-27 00:29:33 -0300

    conversor unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,32 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarunidades(id, valor){
+    if(isNaN(valor)){
+        alert("se ingreso un valor invalido");
+        document.lasunidades.unid_metro.value = "";
+        document.lasunidades.unid_pulgada.value = "";
+        document.lasunidades.unid_pie.value = "";
+        document.lasunidades.unid_yarda.value = "";
+    }else if(id== "metro"){
+        document.lasunidades.unid_pulgada.value = 39.3701*valor;
+        document.lasunidades.unid_pie.value = 3.28084*valor;
+        document.lasunidades.unid_yarda.value = 1.09361*valor;
+    }
+    else if(id== "pulgada"){
+        document.lasunidades.unid_metro.value = 0.0254*valor;
+        document.lasunidades.unid_pie.value = 0.08333333*valor;
+        document.lasunidades.unid_yarda.value = 0.02777778*valor;
+    }
+    else if(id== "pie"){
+        document.lasunidades.unid_metro.value = 0.3048*valor;
+        document.lasunidades.unid_pulgada.value = 12*valor;
+        document.lasunidades.unid_yarda.value = 0.33333333*valor;
+    }
+    else if(id== "yarda"){
+        document.lasunidades.unid_metro.value = 0.9144*valor;
+        document.lasunidades.unid_pie.value = 3*valor;
+        document.lasunidades.unid_pulgada.value = 36*valor;
+    }
+}
