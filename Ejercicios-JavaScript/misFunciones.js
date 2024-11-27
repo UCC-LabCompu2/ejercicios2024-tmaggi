af0967cdda63ff80d940518dd6a2d4e164558f26
Tomas Maggi <tomimaggi2005@gmail.com>
2024-11-27 00:44:04 -0300

    grados a radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -34,3 +34,17 @@ function cambiarunidades(id, valor){
         document.lasunidades.unid_pulgada.value = 36*valor;
     }
 }
+
+function convertirgr(id){
+    var grad, rad;
+    if (id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }
+    else if (id=="radianes"){
+        rad = document.getElementById("radianes").value;
+        grad = (rad*180)/Math.PI;
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
