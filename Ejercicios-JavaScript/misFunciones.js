7a04b4660b5f915eebc0c8921c10d3e5a57d922a
Tomas Maggi <tomimaggi2005@gmail.com>
2024-11-27 01:06:07 -0300

    operaciones

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -57,3 +57,34 @@ function mostarocultar(valorMO) {
         document.getElementById("divMo").style.display= 'none';
     }
 }
+function calcularsuma() {
+    var num1, num2
+
+    num1 = document.getElementsByName("sum_num1")[0].value;
+    num2 = document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value= Number(num1) + Number(num2);
+}
+
+function calcularresta() {
+    var num1, num2
+
+    num1 = document.getElementsByName("res_num1")[0].value;
+    num2 = document.getElementsByName("res_num2")[0].value;
+    document.getElementsByName("res_total")[0].value= Number(num1) - Number(num2);
+}
+
+function calcularmult() {
+    var num1, num2
+
+    num1 = document.getElementsByName("mul_num1")[0].value;
+    num2 = document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].value= Number(num1) * Number(num2);
+}
+
+function calculardiv() {
+    var num1, num2
+
+    num1 = document.getElementsByName("div_num1")[0].value;
+    num2 = document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].value= Number(num1) / Number(num2);
+}
